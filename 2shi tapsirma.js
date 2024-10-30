138d0ed0bf0737c23ad367fd8f08df1fd39c51b3
AlyKoo <alykooabilhan@gmail.com>
2024-10-30 17:37:06 +0600

    Add files via upload

---
+++ 2shi tapsirma.js
@@ -0,0 +1,12 @@
+let notebookName = "Dell G15"; // Ноутбук аты 
+let processor = "13th Gen Intel(R) Core(TM) i5-13450HX"; // процессор аты
+let size = "15.6 FHD 120Hz Display"; // Экран елшемі 
+let color = "Black"; // Ноутбук тусі 
+let todayWeather = 9°; // Казіргі ауа райы 
+let currentTime = 16:33; // Казіргі уакыт
+console. log("notebookName");
+console. log("processor");
+console. log("size");
+console. log("color");
+console. log("todayWeather");
+console. log("currentTime");
