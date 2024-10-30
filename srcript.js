333e462c4dbe8033c25694e9c9890a3ab3dd43c9
AlyKoo <alykooabilhan@gmail.com>
2024-10-30 17:21:15 +0600

    Add files via upload

---
+++ srcript.js
@@ -0,0 +1,11 @@
+let name = "Aly";
+let surname = "Alykoo";
+let birthday = "09.07.02"
+let weight = 66;
+let height = 175; 
+
+console.log("Aty", name )
+console.log("Jasy", age)
+console.log("tugan jili", birthday)
+console.log("salmagy", weight)
+console.log("boiynyn uzyndygy", height)
